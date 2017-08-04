7fb1a4e9f982080720815977e953a999e76d808b
yu1060037020 <1060037020@qq.com>
2017-08-04 21:04:11 +0800

    上传项目到github

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/test/testHelloWorld.java
@@ -0,0 +1,10 @@
+package cn.bdqn.tangcco.tcmp056.test;
+
+/**
+ * Created by Administrator on 2017-08-04.
+ */
+public class testHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
