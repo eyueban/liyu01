e48aebaff321449481946d1e8f75168edebf8f75
yu1060037020 <1060037020@qq.com>
2017-08-04 21:35:01 +0800

    第二次测试修改后上传到github并过滤.idea和*.iml

--- src/main/java/cn/bdqn/tangcco/tcmp056/test/testHelloWorld.java
+++ src/main/java/cn/bdqn/tangcco/tcmp056/test/testHelloWorld.java
@@ -1,10 +1,14 @@
 package cn.bdqn.tangcco.tcmp056.test;
 
+import org.junit.Test;
+
 /**
  * Created by Administrator on 2017-08-04.
  */
 public class testHelloWorld {
-    public static void main(String[] args) {
+
+    @Test
+    public void test() {
         System.out.println("helloworld");
         System.out.println("测试修改后上传到github并过滤.idea和*.iml");
     }
