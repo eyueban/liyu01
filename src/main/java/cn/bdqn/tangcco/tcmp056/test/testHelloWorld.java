abb709efcfb8c3928564756ee5af13dd27f3be93
yu1060037020 <1060037020@qq.com>
2017-08-04 21:16:49 +0800

    测试修改后上传到github并过滤.idea和*.iml

--- src/main/java/cn/bdqn/tangcco/tcmp056/test/testHelloWorld.java
+++ src/main/java/cn/bdqn/tangcco/tcmp056/test/testHelloWorld.java
@@ -6,5 +6,6 @@ package cn.bdqn.tangcco.tcmp056.test;
 public class testHelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("测试修改后上传到github并过滤.idea和*.iml");
     }
 }
